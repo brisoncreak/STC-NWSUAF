76e1a955502bead727d07bb564f9147933ccd8df
liangliangtian <1441335655@qq.com>
2018-06-27 09:49:36 +0800

    yidiangengxin

--- share/views.py
+++ share/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from .models import *
 from index.models import *
 import os
+from django.db.models import F
 # Create your views here.
 #show files
 def show_files(request):
@@ -24,18 +25,21 @@ def upload_file(request):
         if userid != '' and classifyid!='':
             obj = request.FILES.get('inputfile')
             filetype=obj.name.split('.')[1]#获取后缀名
-            file_path = os.path.join('share','upload',obj.name)
-            f = open(file_path, 'wb')
-            for chunk in obj.chunks():
-                f.write(chunk)
-            f.close()
-            File.objects.create(fileName=obj.name,user_id=userid,fileSize=obj.size,fileBeDown=0,file=file_path,fileType=filetype,fileClassify_id=classifyid)
-            return HttpResponse('上传成功')
+            if filetype!='':
+                file_path = os.path.join('share','upload',obj.name)
+                f = open(file_path, 'wb')
+                for chunk in obj.chunks():
+                    f.write(chunk)
+                f.close()
+                File.objects.create(file_name=obj.name,user_id=userid,file_size=obj.size,file_bedown=0,file=file_path,file_type=filetype,file_classify_id=classifyid)
+                return HttpResponseRedirect('/show_file')
+            return HttpResponse('上传文件类型错误')
         return HttpResponse('上传失败')
 #download files
 def download_files(request,fileid):  
     file=File.objects.get(id=fileid)
-    file_name = file.fileName
+    file_name = file.file_name
+    File.objects.filter(id=fileid).update(file_bedown=F('file_bedown')+1)
     file_path = os.path.join('share','upload',file_name) 
     file=open(file_path,'rb')  
     response =FileResponse(file)  
@@ -44,5 +48,10 @@ def download_files(request,fileid):
     return response
 #delete files
 def delete_files(request,fileid):
-    File.objects.get(id=fileid).delete()
+    file=File.objects.get(id=fileid)
+    File.objects.get(id=fileid).delete()   
+    file_name=file.file_name
+    file_path = os.path.join('share','upload',file_name)
+    if os.path.isfile(file_path):
+        os.remove(file_path)
     return HttpResponseRedirect('/show_file')
