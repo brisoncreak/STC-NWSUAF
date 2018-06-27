3f4413a53ea09e4fad5d542341b80e4cb238f18c
zctalk <you@example.com>
2018-06-27 08:43:13 +0800

    增加了整体的基本模板

--- index/urls.py
+++ index/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     url(r'^$', index_views),
     url(r'^login/$', index_login, name='login'),
     url(r'^logout/$', index_logout, name='logout'),
+
+
+    #zc测试
+    url(r'^modelbase/$',index_modelbase),
 ]
--- index/views.py
+++ index/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-
+from .models import *
 
 # Create your views here.
 def index_views(request):
@@ -23,3 +23,16 @@ def index_login(request):
 def index_logout(request):
     logout(request)
     return redirect('/')
+
+
+def index_modelbase(request):
+    nongke = Collegetype.objects.get(title='农科')
+    colleges_nongke = Colleges.objects.filter(classify_id=nongke.id)    
+    gongke = Collegetype.objects.get(title='工科')
+    colleges_gongke = Colleges.objects.filter(classify_id=gongke.id)
+    like = Collegetype.objects.get(title='理科')
+    colleges_like = Colleges.objects.filter(classify_id=like.id)
+    wenke = Collegetype.objects.get(title='文科')
+    colleges_wenke = Colleges.objects.filter(classify_id=wenke.id)
+
+    return render(request,'modelbase.html',locals())
