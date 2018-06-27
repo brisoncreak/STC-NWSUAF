c2fa3ee469d8c0f35a5138cf35cf84d64e6404ce
Ubuntu <tarena@163.com>
2018-06-27 18:17:54 +0800

    下订单业务逻辑

--- market/urls.py
+++ market/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^paying/(\d+)$', paying_views, name='paying'),
     url(r'^add_good/$', add_good_views, name='addgood'),
     url(r'^good_detail/(\d+)$', good_detail_views, name='good_detail'),
+    url(r'^add_order/(\d+)$', new_order_views, name='add_order')
 ]
